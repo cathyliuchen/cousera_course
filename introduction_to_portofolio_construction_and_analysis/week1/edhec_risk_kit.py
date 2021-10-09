4b5cf3788351401181dc0b972eb9a55843d7b5ec
Cathy <cathyliu41@gmail.com>
2021-10-09 21:13:31 +0800

    first commit

---
+++ introduction_to_portofolio_construction_and_analysis/week1/edhec_risk_kit.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+def drawdown(return_series:pd.Series):
+    """
+    Take a times series of asset returns
+    Computes and returns a DataFrame that contains:
+    the wealth index
+    the previous peaks
+    percent drawdowns
+    """
+    wealth_index = 1000*(1+return_series).cumprod()
+    previous_peaks = wealth_index.cummax()
+    drawdowns = (wealth_index - previous_peaks) / previous_peaks
+    return pd.DataFrame({
+        "Wealth": wealth_index,
+        "Peaks": previous_peaks,
+        "Drawdown":drawdowns
+    })
+
+def get_ffme_returns():  
+    """
+    Load the Fama-French Dataset for the returns of the Top and Bottom Deciles by MarktCap
+    """
+    me_m = pd.read_csv("data/Portfolios_Formed_on_ME_monthly_EW.csv",
+            header=0, index_col=0, na_values=-99.99)
+    rets = me_m[['Lo 10', 'Hi 10']]
+    rets.columns = ['SmallCap', 'LargeCap']
+    rets = rets/100
+    rets.index = pd.to_datetime(rets.index, format='%Y%m').to_period('M')
+    return rets
+
+
+def get_hfi_returns():
+    """
+    load and format the EDHEC Hedge Fund Index Returns
+    """
+    hfi = pd.read_csv("data/edhec-hedgefundindices.csv",
+                     header=0, index_col=0, parse_dates=True)
+    hfi = hfi/100
+    hfi.index = hfi.index.to_period('M')
+    return hfi
+    
